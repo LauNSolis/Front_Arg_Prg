bc44717bb387563930ba466e3de90a99a7d444a5
LauNSolis <123700884+LauNSolis@users.noreply.github.com>
2023-02-06 20:41:08 -0300

    Agregué un inicio de sesión de bootstrap, agregué componentes y los llamo mediante routing

--- Arg_Prg_Lautaro_Solis/src/app/app.module.ts
+++ Arg_Prg_Lautaro_Solis/src/app/app.module.ts
@@ -12,7 +12,9 @@ import { EduComponent } from './components/edu/edu.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
-import { HttpClient, HttpClientModule} from "@angular/common/http"
+import { HttpClient, HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { HttpClient, HttpClientModule} from "@angular/common/http"
     ExperienceComponent,
     EduComponent,
     SkillsComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- Arg_Prg_Lautaro_Solis/src/app/components/arg-prg/arg-prg.component.ts
+++ Arg_Prg_Lautaro_Solis/src/app/components/arg-prg/arg-prg.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-arg-prg',
   templateUrl: './arg-prg.component.html',
   styleUrls: ['./arg-prg.component.css']
 })
-export class ArgPrgComponent {
+export class ArgPrgComponent implements OnInit {
+  constructor(private router:Router){}
+  ngOnInit():void {
 
+  }
+  login(){
+    this.router.navigate(['/login'])
+  }
 }
